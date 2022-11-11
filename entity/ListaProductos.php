b780a6f5ef868ced494fb97eb9e67f75ac568474
Giiixx <Giovanni5525@gmail.com>
2022-11-10 21:57:42 -0500

    avance de lista productos

--- entity/ListaProductos.php
+++ entity/ListaProductos.php
@@ -5,6 +5,14 @@
     }
 
     public function _construct($conexion){
-        $
+        $productos_select = $conexion->prepare("SELECT * FROM biologicos");
+        $producto_select->execute();
+        $this->productos = $productos_select->fetchALL(PDO::FETCH_ASSOC);
+    }
+
+    public function init($conexion){
+        $productos_select = $conexion->prepare("SELECT * FROM biologicos");
+        $producto_select->execute();
+        $this->productos = $productos_select->fetchALL(PDO::FETCH_ASSOC);
     }
 ?>
