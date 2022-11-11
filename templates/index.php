2313d6c47b7fff46b034de59f5444dbf48509d1f
antony <antonyorihue@gmail.com>
2022-11-11 17:11:03 -0500

    dawfaw

--- templates/index.php
+++ templates/index.php
@@ -19,62 +19,102 @@
     <title>Document</title>
 </head>
 <body>
-<table >
-        <thead>
-            <tr class="fil_1">
-                <th scope="rowgroup" rowspan="3" >CODIGO</th>
-                <th class ="inmovil" scope="rowgroup" rowspan="3">DESCRIPCION</th>
-                <th scope="rowgroup" rowspan="3" >UNIDAD DE MEDIDA</th>
-                <th scope="colgroup" colspan="4" >INGRESO</th>
+<div>
+     <div class="contenedor_add">
+        <button  class="ref" data-bs-toggle="modal" data-bs-target="#modalForm"><i class="fas fa-plus"></i>&nbsp&nbspAgregar Biologico</button>
+    </div>    
+     <!-- Modal Registrar -->
+     <div class="modal fade m_1" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog m_2">
+            <div class="modal-content m_3">
+                <div class="modal-header m_4">
+                    <h5 class="modal-title" id="exampleModalLabel">Registrar nueva Biologico</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body m_4_1">
+                    <form>
+                        <div class="mb-3">
+                            <label class="form-label">Descripcion Biologico</label>
+                            <select class="comboboxRegistrar" onchange="combobox(<?php echo $i?>)" name="des" id="des<?php echo ($i)?>" >
+                            <?php foreach($productos->productos as $valor=>$value){?>
+                                <option  class="opcion" value="<?php echo $arraydesc[$valor]?>">
+                                <?php echo $productos->getNombre($valor) ?></option>
+                            <?php } ?>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Nombre</label>
+                            <input type="text" class="form-control"  placeholder="Nombre..." required/>
+                        </div>
 
-                <th scope="colgroup" colspan="4" >SALIDAS</th>
+                        <div class="modal-footer d-block btn-block">
+                            <button type="submit" class="btn-agregar float-end"><i class="fas fa-plus"></i>&nbsp&nbspAgregar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                <th scope="colgroup" colspan="3" >DISPONIBLE</th>
+    <div>
+        <table class="datosreporte" >
+            <thead>
+                <tr class="fil_1">
+                    <th scope="rowgroup" rowspan="3" >CODIGO</th>
+                    <th class ="inmovil" scope="rowgroup" rowspan="3">DESCRIPCION</th>
+                    <th scope="rowgroup" rowspan="3" >UNIDAD DE MEDIDA</th>
+                    <th scope="colgroup" colspan="4" >INGRESO</th>
 
-                <th scope="rowgroup" rowspan="3" >Requerimiento mes</th>
-                <th scope="rowgroup" rowspan="3" >Observaciones</th>
-            </tr>
-            <tr class="fil_2">
-                <th scope="rowgroup" rowspan="2" >Saldo del mes anterior (frascos)</th>
-                <th scope="rowgroup" rowspan="2" >Ingresos (frascos)</th>
-                <th scope="rowgroup" rowspan="2" >Ingresos adicionales (frascos)</th>
-                <th scope="rowgroup" rowspan="2" >Total (Saldo + Ingreso) (frascos)</th>
+                    <th scope="colgroup" colspan="4" >SALIDAS</th>
 
-                <th scope="colgroup" colspan="2" >INTERVENCION SANITARIA</th>
-                
-                <th style="width:8%" class="col_2">OTRAS SALIDAS</th>
-                
-                <th scope="rowgroup" rowspan="2" >Total salidas (frascos) (f) d + er</th>
+                    <th scope="colgroup" colspan="3" >DISPONIBLE</th>
 
-                <th scope="rowgroup" rowspan="2" >Saldo final disponible (frascos) (g)  c - f</th>
-                <th scope="rowgroup" rowspan="2" >Fecha de expiracion mas proxima</th>
-                <th scope="rowgroup" rowspan="2" >Lote</th>
-            </tr>
-            <tr class="fil_3">
-                <th >FCO (d)</th>
-                <th >Dosis</th>
-                <th >TRANSFE./DEVOLUCION frascos (e)</th>
-            </tr>
-        </thead>
-        <tbody>
+                    <th scope="rowgroup" rowspan="3" >Requerimiento mes</th>
+                    <th scope="rowgroup" rowspan="3" >Observaciones</th>
+                </tr>
+                <tr class="fil_2">
+                    <th scope="rowgroup" rowspan="2" >Saldo del mes anterior (frascos)</th>
+                    <th scope="rowgroup" rowspan="2" >Ingresos (frascos)</th>
+                    <th scope="rowgroup" rowspan="2" >Ingresos adicionales (frascos)</th>
+                    <th scope="rowgroup" rowspan="2" >Total (Saldo + Ingreso) (frascos)</th>
 
-        <?php for($i=0;$i<54;$i++){?>
-        <tr>
-            <td class="a1">
-                <?php echo $productos->getCodigo($i) ?>
-            </td>
-            <td class="a1">
-                <?php echo $productos->getNombre($i) ?>
-            </td>
-            <td class="a1">
-                <?php echo $productos->getUnidad($i) ?>
-            </td>
-        </tr>
-        <?php } ?> 
-        
+                    <th scope="colgroup" colspan="2" >INTERVENCION SANITARIA</th>
+                    
+                    <th style="width:8%" class="col_2">OTRAS SALIDAS</th>
+                    
+                    <th scope="rowgroup" rowspan="2" >Total salidas (frascos) (f) d + er</th>
 
-        </tbody>
-</table>
+                    <th scope="rowgroup" rowspan="2" >Saldo final disponible (frascos) (g)  c - f</th>
+                    <th scope="rowgroup" rowspan="2" >Fecha de expiracion mas proxima</th>
+                    <th scope="rowgroup" rowspan="2" >Lote</th>
+                </tr>
+                <tr class="fil_3">
+                    <th >FCO (d)</th>
+                    <th >Dosis</th>
+                    <th >TRANSFE./DEVOLUCION frascos (e)</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php foreach($productos->productos as $valor=>$value){?>
+                <tr>
+                    <td class="fil_1_dat">
+                        <?php echo $productos->getCodigo(($valor)) ?>
+                    </td>
+                    <td class="fil_2_dat">
+                        <?php echo $productos->getNombre($valor) ?>
+                    </td>
+                    <td class="fil_3_dat">
+                        <?php echo $productos->getUnidad($valor ) ?>
+                    </td>
+                </tr>
+            <?php } ?> 
+            
+
+            </tbody>
+        </table>
+    </div>
+</div>
     
 </body>
 </html>
