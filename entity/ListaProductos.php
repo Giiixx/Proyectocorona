4d7696ff9dc8024fff76fee22e764064de1353ca
Giiixx <Giovanni5525@gmail.com>
2022-11-10 23:36:49 -0500

    prueba de listado establecido

--- entity/ListaProductos.php
+++ entity/ListaProductos.php
@@ -15,4 +15,23 @@
         $producto_select->execute();
         $this->productos = $productos_select->fetchALL(PDO::FETCH_ASSOC);
     }
+
+    public function getId($position){
+        return $this->productos[$position]['idBiologicos'];
+    }
+    public function getCodigo($position){
+        return $this->productos[$position]['BiologicosdesCod']
+    }
+    public function getNombre($position){
+        return $this->productos[$position]['BiologicosNom'];
+    }
+    public function getProporcion($position){
+        return $this->productos[$position]['BiologicosProporcion']
+    }
+    public function getUnidad($position){
+        return $this->productos[$position]['BiologicosUnidad'];
+    }
+    public function getId_cat($position){
+        return $this->productos[$position]['Categoria_idCategoria']
+    }
 ?>
--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,12 @@
+<?php
+    require '../conections/basededatos.php';
+    require '../entity/ListaProductos.php';
+
+    $productos = new ListaProductos($conn);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,7 +56,24 @@
                 <th >TRANSFE./DEVOLUCION frascos (e)</th>
             </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+
+        <?php for($i=0;$i<54;$i++){?>
+            <tr>
+                <td class="a1">
+                    <?php echo $producto->getCodigo($i) ?>
+                </td>
+                <td class="a1">
+                    <?php echo $producto->getNombre($i) ?>
+                </td>
+                <td class="a1">
+                    <?php echo $producto->getUnidad($i) ?>
+                </td>
+            </tr>
+            <?php } ?>
+
+            
+        </tbody>
 </table>
     
 </body>
